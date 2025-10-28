cc694c86a99f6d1ed06fe3486f504fbbd5849610
Phạm Vũ Hoài Phương Thảo <thaomai22102005@gmail.com>
2025-10-29 00:21:04 +0700

    add /api to swagger config

--- src/main/java/swp391/fa25/saleElectricVehicle/config/SwaggerConfig.java
+++ src/main/java/swp391/fa25/saleElectricVehicle/config/SwaggerConfig.java
@@ -27,6 +27,6 @@ public class SwaggerConfig {
                                         .scheme("bearer")
                                         .bearerFormat("JWT")))
                 // <-- set server url to https
-                .addServersItem(new Server().url("https://tiembanhvuive.io.vn"));
+                .addServersItem(new Server().url("https://tiembanhvuive.io.vn/api"));
     }
 }
